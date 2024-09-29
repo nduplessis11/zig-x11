dfbf33538c84c439b2c855737efe851ebe1ccfa1
Nick DuPlessis <nduplessis15@gmail.com>
2024-09-29 10:15:51 -0400

    Get DISPLAY, add handle_error()

--- src/main.zig
+++ src/main.zig
@@ -5,6 +5,9 @@ const net = std.net;
 pub fn main() !void {
     std.log.info("Starting windowing system...", .{});
 
+    const display = posix.getenv("DISPLAY") orelse ":0";
+    std.debug.print("{s}\n", .{display});
+
     const stream = try net.connectUnixSocket("/tmp/.X11-unix/X1");
     errdefer stream.close();
 
@@ -23,9 +26,14 @@ pub fn main() !void {
     // Check for success (first byte should be 1)
     if (reply[0] == 1) {
         std.log.info("Connected to X server", .{});
+        handle_error(&reply);
     } else {
         std.log.err("Failed to connect to X server", .{});
     }
 
     stream.close();
+}
+
+fn handle_error(reply: []u8) void {
+    std.debug.print("Version {} \n", .{reply[2]});
 }
